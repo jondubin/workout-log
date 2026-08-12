a5d915314255d83e37cac0c2dde4b18e4ab5349d
jondubin <jonathandubin@gmail.com>
2026-08-12 11:36:24 -0400

    Always build workout assets for project path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/workout/" : "/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/workout/" : "/",
   plugins: [react()],
   server: {
     port: 3000,
     strictPort: true,
   },
-});
+}));
